5695c9a17d95a309ac84f2d9a49a454593c4479f
ademola <fioyedemola1@gmil.com>
2021-07-30 01:28:04 +0100

    day3

--- day2/test.py
+++ day2/test.py
@@ -1,1 +1,12 @@
-an example
+sample = [1,2,3,3,4,4,5,6,6,7,6,54] #iterated
+
+# first = sample[0]
+# second = sample[1]
+# third = sample[2]
+
+# first,*_,last =sample
+another = [*sample]
+print(another)
+
+print(len(another))
+print(len(sample))
